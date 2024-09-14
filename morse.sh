8cfb6f1dcca1a1f60ab21f93ac40407b4fa471b2
Sebastian <78531972+SebastianCaron@users.noreply.github.com>
2024-09-14 16:01:27 +0200

    Ajout de la création des tables de correspondances
    Ajout de la création des tables de correspondances en utilisant une expression reg.

--- morse.sh
+++ morse.sh
@@ -3,4 +3,19 @@
 declare -A code_morse # Lettre : morse
 declare -A decode_morse # Morse : Lettre
 
+# Construction des Tables
+cat 'morse.json' | while  read ligne
+do
+    # echo $ligne
+    # /"+."+|"+[_.]{1,6}"+/
+    if [[ $ligne =~ \"([^\"]+)\"[[:space:]]*:[[:space:]]*\"([^\"]+)\" ]]
+    then
+        # echo $ligne
+        cle="${BASH_REMATCH[1]}" # Premier Groupe entre ()
+        valeur="${BASH_REMATCH[2]}" # Deuxieme Groupe entre ()
+        
+        code_morse["$cle"]="$valeur"
+        decode_morse["$valeur"]="$cle"
+    fi
+done
 
