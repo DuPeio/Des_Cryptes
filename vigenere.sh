23079a4a63752ad0cc0e6f88600ae31ed4b7c0f5
Dylan <dydy86100@gmail.com>
2024-09-28 13:26:09 +0200

    déplacement des tests

--- vigenere.sh
+++ vigenere.sh
@@ -228,11 +228,3 @@ chiffrementVigenere() {
 dechiffrementVigenere() {
     echo "test"
 }
-
-# vigenereMain
-# genCle
-# choixCle
-
-# chiffrementVigenere "castor" "lorem ipsum dolor"
-# chiffrementVigenere "CaSt69420or/." "lorem ipsum dolor"
-# chiffrementVigenere "69450" "lorem ipsum dolor"
