b4b61cd3726b92d66f2cadc71d1bce575d98ab17
Dylan <dydy86100@gmail.com>
2024-10-14 21:59:17 +0200

    Update testVigenere.sh

--- testVigenere.sh
+++ testVigenere.sh
@@ -12,5 +12,5 @@ source ./vigenere.sh
 # chiffrementVigenere "castor" "lorem ipsum dolor"
 # chiffrementVigenere "CaSt69420or/." "lorem ipsum dolor"
 # chiffrementVigenere "69450" "lorem ipsum dolor"
-chiffrementVigenere "castor" "Ma phrase 69"
+# chiffrementVigenere "castor" "Ma phrase 69"
 # dechiffrementVigenere "castor" "Oa hafrue 48"
